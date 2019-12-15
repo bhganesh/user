a950f201bf31882315c1b6126e09db1ad1d3e0a0
praveen-edulakanti <praveen.edulakanti@gmail.com>
2019-12-15 19:35:26 +0530

    db connection error blocking complete page

--- config.php
+++ config.php
@@ -11,6 +11,6 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    echo "ERROR: Could not connect. " . mysqli_connect_error();
 }
 ?>
